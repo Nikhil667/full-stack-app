de86a2e0c240f390917c4e0e9ddd8f84ff4152fc
Java With Nikhil <nikhilmimroth2@gmail.com>
2023-03-12 23:17:41 +0530

    Backend Complete With Auth

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const dbConnect = require("./database/dbConnect");
+const User = require("./database/userModel");
+
+const bcrypt = require("bcrypt");
+
+const jwt = require("jsonwebtoken");
+
+const auth = require("./auth");
 
 // body parser configuration
 app.use(bodyParser.json());
@@ -11,11 +18,124 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 dbConnect();
 
-app.get("/", (request, response, next) => {
-  response.json({ message: "Hey! Thiasdsds is your server response!" });
+// Curb Cores Error by adding a header here
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
   next();
 });
 
+
+app.post("/register", (request, response) => {
+    bcrypt
+    .hash(request.body.password, 10)
+    .then((hashedPassword) => {
+      // create a new user instance and collect the data
+      const user = new User({
+        email: request.body.email,
+        password: hashedPassword,
+      });
+
+      // save the new user
+      user
+        .save()
+        // return success if the new user is added to the database successfully
+        .then((result) => {
+          response.status(201).send({
+            message: "User Created Successfully",
+            result,
+          });
+        })
+        // catch error if the new user wasn't added successfully to the database
+        .catch((error) => {
+          response.status(500).send({
+            message: "Error creating user",
+            error,
+          });
+        });
+    })
+    // catch error if the password hash isn't successful
+    .catch((error) => {
+      response.status(500).send({
+        message: "Password was not hashed successfully",
+        error,
+      });
+    });
+  
+});
+
+app.post("/login", (request, response) => {
+  // check if email exists
+  User.findOne({ email: request.body.email })
+  // if email exists
+  .then((userFound) => {
+    // compare the password entered and the hashed password found
+    bcrypt.compare(request.body.password, userFound.password)
+     // if the passwords match
+    .then((passwordCheck) => {
+      // check if password matches
+      if(!passwordCheck){
+        return response.status(400).send({
+          message: "Password does not match",
+          error,
+        })
+      }
+      //   create JWT token
+      const token = jwt.sign(
+        {
+          userId: userFound._id,
+          userEmail: userFound.email,
+        },
+        "RANDOM-TOKEN",
+        {
+          expiresIn: "24h"
+        }
+      )
+      //   return success response
+      response.status(200).send({
+        message: "Login Successful",
+        email: userFound.email,
+        token,
+      });
+    })
+    // catch error if password does not match
+    .catch((error) => {
+      response.status(400).send({
+        message: "Passwords does not match",
+        error,
+      });
+    })
+  })
+  // catch error if email does not exist
+  .catch((e) => {
+    response.status(404).send({
+      message: "Email not found",
+      e,
+    });
+  });
+})
+
+app.get("/", (request, response, next) => {
+  response.json({ message: "Hey! This is your server response!" });
+  next();
+});
+
+// free endpoint
+app.get("/free-endpoint", (request, response) => {
+  response.json({ message: "You are free to access me anytime" });
+});
+
+// authentication endpoint
+app.get("/auth-endpoint", auth, (request, response) => {
+  response.json({ message: "You are authorized to access me" });
+});
 
 
 module.exports = app;
